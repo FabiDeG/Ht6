41f0f1073e1a926bde17ab62f6b8931b2681e0bf
Fabiola <105880646+FabiDeG@users.noreply.github.com>
2023-03-06 22:22:19 -0600

    Driver cases

--- src/Main/DriverP.java
+++ src/Main/DriverP.java
@@ -20,37 +20,134 @@ public class DriverP {
 			
 			switch (opc) {
 			case  1: {
+				
+				int res = 0;
+				
+				do {
 				System.out.println("Quiere:");
 				System.out.println("1. Agregar un producto");
 				System.out.println("2. Mostrar la categoria del producto");
 				System.out.println("3. Mostrar los datos del producto, categoria y la cantidad de cada articulo");
-				System.out.println("Mostrar los datos del producto, categoría y la cantidad de cada artículo que el usuario tiene en su\r\n"
+				System.out.println("4. Mostrar los datos del producto, categoría y la cantidad de cada artículo que el usuario tiene en su\r\n"
 						+ "colección, ordenadas por tipo");
-				System.out.println("4. Mostrar el producto y la categoría de todo el inventario");
-				System.out.println("Mostrar el producto y la categoría existentes, ordenadas por tipo");
+				System.out.println("5. Mostrar el producto y la categoría de todo el inventario");
+				System.out.println("6. Mostrar el producto y la categoría existentes, ordenadas por tipo");
+				
+				switch (res) {
+				case 1: {
+				
+				}break;
+				
+				case 2: {
+					
+				}break;
+				
+				case 3: {
+					
+				}break;
+				
+				case 4: {
+					
+				}break;
+				
+				case 5: {
+					
+				}break;
+				
+				case 6: {
+					
+				}break;
+				
+				}
+				
+				}while (res != 6);
 			}break;
 			
 			case 2: {
+int res = 0;
+				
+				do {
 				System.out.println("Quiere:");
 				System.out.println("1. Agregar un producto");
 				System.out.println("2. Mostrar la categoria del producto");
 				System.out.println("3. Mostrar los datos del producto, categoria y la cantidad de cada articulo");
-				System.out.println("Mostrar los datos del producto, categoría y la cantidad de cada artículo que el usuario tiene en su\r\n"
+				System.out.println("4. Mostrar los datos del producto, categoría y la cantidad de cada artículo que el usuario tiene en su\r\n"
 						+ "colección, ordenadas por tipo");
-				System.out.println("4. Mostrar el producto y la categoría de todo el inventario");
-				System.out.println("Mostrar el producto y la categoría existentes, ordenadas por tipo");
-			}
+				System.out.println("5. Mostrar el producto y la categoría de todo el inventario");
+				System.out.println("6. Mostrar el producto y la categoría existentes, ordenadas por tipo");
+				
+				switch (res) {
+				case 1: {
+				
+				}break;
+				
+				case 2: {
+					
+				}break;
+				
+				case 3: {
+					
+				}break;
+				
+				case 4: {
+					
+				}break;
+				
+				case 5: {
+					
+				}break;
+				
+				case 6: {
+					
+				}break;
+				
+				}
+				
+				}while (res != 6);
+			}break;
 			
 			case 3: {
+int res = 0;
+				
+				do {
 				System.out.println("Quiere:");
 				System.out.println("1. Agregar un producto");
 				System.out.println("2. Mostrar la categoria del producto");
 				System.out.println("3. Mostrar los datos del producto, categoria y la cantidad de cada articulo");
-				System.out.println("Mostrar los datos del producto, categoría y la cantidad de cada artículo que el usuario tiene en su\r\n"
+				System.out.println("4. Mostrar los datos del producto, categoría y la cantidad de cada artículo que el usuario tiene en su\r\n"
 						+ "colección, ordenadas por tipo");
-				System.out.println("4. Mostrar el producto y la categoría de todo el inventario");
-				System.out.println("Mostrar el producto y la categoría existentes, ordenadas por tipo");
-			}
+				System.out.println("5. Mostrar el producto y la categoría de todo el inventario");
+				System.out.println("6. Mostrar el producto y la categoría existentes, ordenadas por tipo");
+				
+				switch (res) {
+				case 1: {
+				
+				}break;
+				
+				case 2: {
+					
+				}break;
+				
+				case 3: {
+					
+				}break;
+				
+				case 4: {
+					
+				}break;
+				
+				case 5: {
+					
+				}break;
+				
+				case 6: {
+					
+				}break;
+				
+				}
+				
+				}while (res != 6);
+			}break;
 			}
 		
 		
